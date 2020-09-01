1cb3cf41a91e7bc3a42a29f2eabef4208b70eaf0
Louise <louise.gilligan@zoopla.co.uk>
2020-09-01 16:24:52 +0100

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import './header.scss';
+import { Header } from './Header';
+import { Footer } from './Footer';
+import {
+  BrowserRouter as Router, Switch, Route, Link, useParams
+} from "react-router-dom";
+import {BooksList} from "./Bookslist"
 
-function App() {
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header></Header>
+      <Switch>
+        <Route exact path='/'>
+          <HomePage />
+        </Route>
+        <Route exact path="/books">
+          <BooksList />
+        </Route>
+        <Route path="/books/:id">
+          <ManageBooks />
+        </Route>
+        <Route path="/users">
+          <Members />
+        </Route>
+        <Route path="/users/:id">
+          <ManageMembers />
+        </Route>
+      </Switch>
+      <Footer></Footer>
+    </Router>
   );
 }
+function HomePage() {
+  return <h2>Home</h2>;
+}
+
+function ManageBooks() {
+  let { id } = useParams();
+return <h2>Manage Books {id}</h2>;
+}
+
+function Members() {
+  return <h2>Members</h2>;
+}
+function ManageMembers() {
+  return <h2>Manage Members</h2>;
+}
+
 
 export default App;
+
+
+
